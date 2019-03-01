3510142ef3e01d49e3ae670aa39961b8dee7bfec
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-01 11:51:38 -0800

    passing body and title reducer tests

--- src/reducers/notes.test.js
+++ src/reducers/notes.test.js
@@ -1,11 +1,12 @@
 import reducer from './notes';
+import { ADD_NOTE, UPDATE_NOTE_TITLE, UPDATE_NOTE_BODY } from '../actions/notes';
 
 describe('notes reducer', () => {
   it('can add a note', () => {
     const state = {};
 
     const addedNote = reducer(state, {
-      type: 'ADD_NOTE',
+      type: ADD_NOTE,
       payload: {
         title: 'yolo420',
         body: 'when'
@@ -17,6 +18,43 @@ describe('notes reducer', () => {
         title: 'yolo420',
         body: 'when'
       }
+    });
+  });
+
+  it('can update a note title', () => {
+    const state = {
+      title: '',
+      body: ''
+      
+    };
+
+    const noteTitle = reducer(state, {
+      type: UPDATE_NOTE_TITLE,
+      payload: 'yolo420',
+    });
+
+    expect(noteTitle).toEqual({
+      title: 'yolo420',
+      body: ''
+    });
+  });
+
+
+  it('can update a note body', () => {
+    const state = {
+      title: '',
+      body: ''
+      
+    };
+
+    const noteBody = reducer(state, {
+      type: UPDATE_NOTE_BODY,
+      payload: 'yolo420',
+    });
+
+    expect(noteBody).toEqual({
+      title: '',
+      body: 'yolo420'
     });
   });
 });
