2cdb784dfbb4554a4874f63b4ecf7e6a5491fe21
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-01 11:34:51 -0800

    passing get note title selector test

--- src/selectors/notes.js
+++ src/selectors/notes.js
@@ -1,3 +1,7 @@
 export const getNotes = (state, id) => {
   return { ...state[id], id };
 };
+
+export const getNoteTitle = (state, id) => {
+  return state[id].title;
+};
--- src/selectors/notes.test.js
+++ src/selectors/notes.test.js
@@ -1,4 +1,4 @@
-import { getNotes } from './notes';
+import { getNotes, getNoteTitle } from './notes';
 
 describe('notes selector', () => {
   it('gets a list of notes', () => {
@@ -15,5 +15,17 @@ describe('notes selector', () => {
       id: 1234,
       body: 'expert-fiesta'
     });
+  });
+
+  it('gets a note title', () => {
+    const state = {
+      1234: {
+        title: 'yolo420',
+        body: 'expert-fiesta'
+      }
+    };
+
+    const selectedTitle = getNoteTitle(state, 1234);
+    expect(selectedTitle).toEqual('yolo420');
   });
 });
